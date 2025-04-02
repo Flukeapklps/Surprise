bccf49120f84033cb773557540844202a1372868
Flukeapklps <fluke1847@gmail.com>
2025-04-02 19:25:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ document.getElementById("showVideoBtn").addEventListener("click", function() {
     document.getElementById("videoContainer").classList.remove("hidden");
     this.classList.add("hidden");
 });
+
+document.getElementById("backButton").addEventListener("click", function() {
+    // Hide the video container and show the surprise message again
+    document.getElementById("videoContainer").classList.add("hidden");
+    document.getElementById("surpriseMessage").classList.remove("hidden");
+    document.getElementById("showVideoBtn").classList.remove("hidden");
+    this.classList.add("hidden");
+});
